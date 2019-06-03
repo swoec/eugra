d38a5f990f50027aa4df601c9532b2acf6fa6e41
swoec <swoec.wang@gmail.com>
2019-06-03 16:21:19 +1200

    setting

--- eugra/settings.py
+++ eugra/settings.py
@@ -95,8 +95,14 @@ WSGI_APPLICATION = 'eugra.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "mxshop",
+        'USER': 'root',
+        'PASSWORD': "123456",
+        'HOST': "127.0.0.1",
+        'OPTIONS': {
+            'init_command': 'SET default_storage_engine=INNODB',
+        }
     }
 }
 
